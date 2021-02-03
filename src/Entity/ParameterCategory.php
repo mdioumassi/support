fc51991fdde45ef111d0f6f6a48d39e9c2ee25c4
mdioumassi <mdioumassi@yahoo.fr>
2021-02-03 19:54:52 +0100

    gitignore

--- src/Entity/ParameterCategory.php
+++ src/Entity/ParameterCategory.php
@@ -81,4 +81,16 @@ class ParameterCategory
         return $this;
     }
 
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
 }
